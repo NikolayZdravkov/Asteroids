1847de6b523c19b78b6c16ece941a3466a123a87
niketzaBG <zdravkovnikolay5@gmail.com>
2023-11-22 22:18:23 +0200

    Implemented Player Drawing

--- Asteroids.cpp
+++ Asteroids.cpp
@@ -2,11 +2,45 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+class Player 
+{
+public:
+    Player()
+        : position(500, 500), angle(45), array(sf::Quads, 4)
+    {
+        array[0].position = sf::Vector2f(0, -40);
+        array[1].position = sf::Vector2f(-30, 30);
+        array[2].position = sf::Vector2f(0, 15);
+        array[3].position = sf::Vector2f(30, 30);
+
+        for (size_t i = 0; i < array.getVertexCount(); i++)
+        {
+            array[i].color = sf::Color::White;
+        }
+    }
+
+    void Draw(sf::RenderWindow& window) 
+    {
+        sf::Transform transform;
+        transform.translate(position).rotate(angle);
+        window.draw(array, transform);
+    }
+
+    sf::Vector2f position;
+    float angle;
+
+private:
+    sf::VertexArray array;
+
+};
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1200, 900), "Asteroids", 
         sf::Style::Close | sf::Style::Titlebar);
     sf::Clock clock;
+
+    Player player;
 
     while (window.isOpen()) 
     {
@@ -23,7 +57,9 @@ int main()
         //update our game
 
         window.clear();
-        // draw the game..
+        
+        player.Draw(window);
+
         window.display();
     }
 }
